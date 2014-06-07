a9d7796b40118c20aa8437275fde5fb076c5726f
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-06-07 13:41:50 +0200

    Adding access public data by user id

--- test/test_access_public.js
+++ test/test_access_public.js
@@ -20,4 +20,22 @@ describe('Accessing public data', function(){
 			});
 		});
 	});
+	
+	describe('Calling .graph with /4 without access_token', function(){
+		it('should return public info about Mark Zuckerberg', function(done) {
+			var fb = new fbgraphapi.Facebook(null);
+			fb.graph('/4', function(err, res) {
+				expected = { id: '4',
+						  first_name: 'Mark',
+						  gender: 'male',
+						  last_name: 'Zuckerberg',
+						  link: 'https://www.facebook.com/zuck',
+						  locale: 'en_US',
+						  name: 'Mark Zuckerberg',
+						  username: 'zuck' };
+				assert.deepEqual(res, expected);
+				done();
+			});
+		});
+	});
 });
