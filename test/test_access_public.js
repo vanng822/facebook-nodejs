2d4beb0c9a4334b53b88dea71eaaa0e9e23f5623
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-03-04 19:17:17 +0100

    Support api call with version. One could build the path with the version but some methods didn't support it.

--- test/test_access_public.js
+++ test/test_access_public.js
@@ -3,37 +3,34 @@ var assert = require('assert');
 var fbgraphapi = require('../index.js');
 
 describe('Accessing public data', function(){
-	describe('Calling .graph with /zuck without access_token', function(){
-		it('should return public info about Mark Zuckerberg', function(done) {
+	describe('Calling .graph with /1155678417 without access_token with no version', function(){
+		it('should return public info about the author', function(done) {
 			var fb = new fbgraphapi.Facebook(null);
-			fb.graph('/zuck', function(err, res) {
-				expected = { id: '4',
-						  first_name: 'Mark',
+			fb.graph('/1155678417', function(err, res) {
+				var expected = { id: '1155678417',
+						  first_name: 'Van Nhu',
 						  gender: 'male',
-						  last_name: 'Zuckerberg',
-						  link: 'https://www.facebook.com/zuck',
+						  last_name: 'Nguyen',
 						  locale: 'en_US',
-						  name: 'Mark Zuckerberg',
-						  username: 'zuck' };
+						  name: 'Van Nhu Nguyen',
+						  username: 'nguyen.van.nhu' };
 				assert.deepEqual(res, expected);
 				done();
 			});
 		});
 	});
 	
-	describe('Calling .graph with /4 without access_token', function(){
-		it('should return public info about Mark Zuckerberg', function(done) {
-			var fb = new fbgraphapi.Facebook(null);
-			fb.graph('/4', function(err, res) {
-				expected = { id: '4',
-						  first_name: 'Mark',
-						  gender: 'male',
-						  last_name: 'Zuckerberg',
-						  link: 'https://www.facebook.com/zuck',
-						  locale: 'en_US',
-						  name: 'Mark Zuckerberg',
-						  username: 'zuck' };
-				assert.deepEqual(res, expected);
+	describe('Calling .graph with /1155678417 without access_token with version v2.2', function(){
+		it('should return "An access token is required to request this resource."', function(done) {
+			var fb = new fbgraphapi.Facebook(null, 'v2.2');
+			fb.graph('/1155678417', function(err, res) {
+				expected = {
+				  error_subcode: null,
+				  type: 'OAuthException',
+				  code: 104,
+				  statusCode: 400,
+				  message: 'An access token is required to request this resource.' }
+				assert.deepEqual(err, expected);
 				done();
 			});
 		});
