7f60f20677beb65ef22375919480bd94f351719a
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2016-05-13 21:12:36 +0200

    Use nock for mocking response

--- test/test_access_public.js
+++ test/test_access_public.js
@@ -1,13 +1,30 @@
 
 var assert = require('assert');
 var fbgraphapi = require('../index.js');
+var nock = require('nock');
+nock('https://graph.facebook.com')
+.get('/v1.0/1155678417?')
+.reply(200, { id: '1155678417',
+			first_name: 'Van Nhu',
+			gender: 'male',
+			last_name: 'Nguyen',
+			locale: 'en_US',
+			name: 'Van Nhu Nguyen',
+			username: 'nguyen.van.nhu' });
+
+nock('https://graph.facebook.com')
+.get('/v2.2/1155678417?')
+.reply(400, {error: {
+	error_subcode: null,
+	type: 'OAuthException',
+	code: 104,
+	message: 'An access token is required to request this resource.' }});
 
 describe('Accessing public data', function(){
 	describe('Calling .graph with /1155678417 without access_token with version v1.0', function(){
 		it('should return public info about the author', function(done) {
 			var fb = new fbgraphapi.Facebook(null, 'v1.0');
 			fb.graph('/1155678417', function(err, res) {
-				console.log(err)
 				var expected = { id: '1155678417',
 						  first_name: 'Van Nhu',
 						  gender: 'male',
@@ -20,7 +37,7 @@ describe('Accessing public data', function(){
 			});
 		});
 	});
-	
+
 	describe('Calling .graph with /1155678417 without access_token with version v2.2', function(){
 		it('should return "An access token is required to request this resource."', function(done) {
 			var fb = new fbgraphapi.Facebook(null, 'v2.2');
